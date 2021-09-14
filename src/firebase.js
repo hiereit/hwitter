1eb6a27c7ca52ba0c3395a8b492c0b034ce81382
hiereit <nasera98@naver.com>
2021-09-14 22:25:47 +0900

    3-1장 커밋완료

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCXzmITO5dUVD-AqV3iUcuMWmDhnalCZlc",
-    authDomain: "hwitter-47051.firebaseapp.com",
-    projectId: "hwitter-47051",
-    storageBucket: "hwitter-47051.appspot.com",
-    messagingSenderId: "384394626370",
-    appId: "1:384394626370:web:0d890fa76c0980bad374db",
-    measurementId: "G-EYLN92KD62"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId:  process.env.REACT_APP_APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
   };
 
 export default firebase.initializeApp(firebaseConfig);
