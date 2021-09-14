32ca2b18084e07b6dcdd41f19ff9f2159c7479a4
hiereit <nasera98@naver.com>
2021-09-14 22:10:52 +0900

    2장 커밋완료

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCXzmITO5dUVD-AqV3iUcuMWmDhnalCZlc",
+    authDomain: "hwitter-47051.firebaseapp.com",
+    projectId: "hwitter-47051",
+    storageBucket: "hwitter-47051.appspot.com",
+    messagingSenderId: "384394626370",
+    appId: "1:384394626370:web:0d890fa76c0980bad374db",
+    measurementId: "G-EYLN92KD62"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
