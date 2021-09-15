8cf6f666841854f61bd65f678f998c919c0d4c84
hiereit <nasera98@naver.com>
2021-09-15 17:44:31 +0900

    4장 완료 커밋

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
-import AppRouter from "./Router";
+import { useEffect, useState } from "react";
+import AppRouter from "components/Router";
+import  { authService } from "fbase";
 
 function App() {
-  return <AppRouter />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user){
+        setIsLoggedIn(user);
+      }else{
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  //setInterval(() => console.log(authService.currentUser), 2000); //코드 실행후 2초마다 authService.currentUser의 값을 출력
+
+  //console.log(authService.currentUser);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."}
+      <footer>&copy;{new Date().getFullYear()} Hwitter</footer>
+    </>
+  );
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,90 @@
-const Auth = () => <span>Auth</span>;
+import { useState } from "react";
+import { authService, firebaseInstance } from "fbase";
+
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError]  = useState("");
+    const toggleAccount = () => setNewAccount((prev)  => ! prev);
+
+    const onSocialClick = async (event) => {
+        //console.log(event.target.name);
+        const{
+            target: {name},
+        } = event;
+        let provider;
+        if(name === "google"){
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        }else if(name === "github"){
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+        }
+        const data = await authService.signInWithPopup(provider);
+        console.log(data);
+    };
+    
+    const onChange = (event) => {
+        //console.log(event.target.name);
+
+        //로그인폼 상태 업데이트
+        const {
+            target : { name, value },
+        } = event;
+        if(name === "email"){
+            setEmail(value);
+        }else if (name === "password"){
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            let data;
+            if (newAccount){
+                //create new Account
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            }else{
+                //login
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        }catch(error){
+            // console.log(error);
+            setError(error.message);
+        }
+    };
+
+    return (
+        //login 기본구조만들기
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    name="email" 
+                    type="email" placeholder="Email" required
+                    value={email}
+                    onChange={onChange}
+                    />
+                <input 
+                    name="password"
+                    type="password" placeholder="Password" required
+                    value={password}
+                    onChange={onChange}
+                    />
+                <input type="submit" value={newAccount ? "Create Account" : "Login"}/>
+                {error}
+            </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign In" : "Create Account"}
+            </span>
+            <div>
+                <button onClick={onSocialClick} name="google">
+                    Continue with Google</button>
+                <button onClick={onSocialClick} name="github">
+                    Continue with Github</button>
+            </div>
+        </div>    
+    );
+};
 
 export default Auth;
